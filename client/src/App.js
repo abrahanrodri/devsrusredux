d2c6b1499ecfe466db291adec8a7889b9b4b3b01
stephengallo1977 <stephengallo1977@gmail.com>
2019-09-03 17:51:07 -0400

    tutoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import Navbar from "./Componets/Nav";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Wrapper from "./Componets/Wrapper"
-import HomePage from "./Componets/CreateEvent";
+import React from "react";
+import Navbar from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Wrapper from "./components/Wrapper";
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-<div>
-  <Router>
-<div>
-  <Navbar />
-  <Wrapper>
-      <Route path ="/" component ={HomePage} />
-      <Route path ="/Comments" component ={Comments} />
-      <Route path ="/CreateEvent" component ={CreateEvent} />
-  </Wrapper>
-</div>
-
-  </Router>
-</div>
+    <div>
+      <Router>
+        <div>
+          <Navbar />
+          <Wrapper>
+            <Route path="/" component={HomePage} />
+            {/* <Route path="/Comments" component={Comments} />
+            <Route path="/CreateEvent" component={CreateEvent} /> */}
+          </Wrapper>
+        </div>
+      </Router>
+    </div>
   );
 }
 
