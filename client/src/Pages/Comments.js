53d43593f5a73635d157051d26653732c3c1b750
Marie  Pepe <mariepepe@Maries-MacBook-Air.local>
2019-09-05 14:34:13 -0400

    updating comments.js

--- client/src/Pages/Comments.js
+++ client/src/Pages/Comments.js
@@ -1,28 +1,87 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { Grid, Paper, TextField } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-  input: {
-    display: 'none',
-  },
-}));
 
-export default function ContainedButtons() {
-  const comments = useStyles();
+const styles = theme => ({
+    paper: {
+      margin: "5%",
+      textAlign: 'center',
+    },
+    textField: {
+      color: "red"
+    }
+  });
+  
+  class comments extends React.Component {
+    state = {
+      someItem1: "",
+      initials: "",
+      comments: ""
+    }
+  
+    handleChange = (event) => {
+      console.log(event.target.name, event.target.value)
+      const { name, value } = event.target;
+      this.setState({ [name]: value })
+    }
 
-  return (
-    <div>
-      <Button variant="contained" className={classes.comments}>
-        Default
-      </Button>
-      <comments variant="contained" href="#contained-buttons" className={classes.comments}>
-      Link
-    </comments>
-    </div>
-    );
-}
+    handleDelete = () => {
+     this.setState({ deleteClicked: [this.state.deleteClicked], comments: this.state.delete});
+     alert('deleting')
+    }
+
+    handlePost = () => {
+    this.setState({ postClick:[this.state.postClick], comments: this.state.post});
+    }
+  
+    render() {
+      const { classes } = this.props;
+      return (
+
+    
+        <div className={classes}>
+          <Paper className={classes.paper}>
+            <Grid container>
+              <Grid item xs={12}>
+                <h1>comments</h1>
+  
+                <TextField
+                  fullWidth
+                  id="standard-name"
+                  label="Name Label"
+                  name="someItem1"
+                  className={classes.textField}
+                  value={this.state.someItem1}
+                  onChange={this.handleChange}
+                  margin="normal"
+                />
+                
+                <TextField
+                  fullWidth
+                  id="standard-name"
+                  label="initials"
+                  name="initials"
+                  className={classes.textField}
+                  value={this.state.initials}
+                  onChange={this.handleChange}
+                  margin="normal"
+                />
+                <button onClick={this.handlePost} color="">Post</button>
+
+                <button onClick={this.handleDelete} color="primary">Delete</button>
+  
+  
+  
+              </Grid>
+            </Grid>
+          </Paper>
+        </div>
+      );
+    }
+  }
+
+  
+  export default withStyles(styles)(comments);
 
