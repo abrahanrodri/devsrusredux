a84015f8ed7efe761cda586d830074050c773911
Ayanna Burrowes <a.burrowes529@gmail.com>
2019-09-05 13:18:11 -0400

    ayanna: updating

--- client/src/Pages/CreateEvent.js
+++ client/src/Pages/CreateEvent.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import { Grid, Paper, TextField } from '@material-ui/core';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid, Paper, TextField } from "@material-ui/core";
+import "./css/createEvent.css";
 
 const styles = theme => ({
   paper: {
     margin: "5%",
-    textAlign: 'center',
-    height: '300px',
+    textAlign: "center",
+    height: "700px"
   },
   textField: {
-    margin: '5%',
-    color: "black",
+    color: "black"
+    // width: "100%"
   }
-
 });
 
 class CreateEvent extends React.Component {
   state = {
     Item: ""
-  }
+  };
 
-  handleChange = (event) => {
-    console.log(event.target.name, event.target.value)
+  handleChange = event => {
+    console.log(event.target.name, event.target.value);
     const { name, value } = event.target;
-    this.setState({ [name]: value })
-  }
+    this.setState({ [name]: value });
+  };
 
   render() {
     const { classes } = this.props;
@@ -32,10 +32,10 @@ class CreateEvent extends React.Component {
       <div className={classes.root}>
         <Paper className={classes.paper}>
           <Grid container>
-            <Grid item xs={12} sm = {6}>
+            <Grid item xs={12} sm={6}>
               <h1>Create Event</h1>
 
-              <TextField
+              {/* <TextField
                 fullWidth
                 id="standard-name"
                 label="Event"
@@ -44,10 +44,64 @@ class CreateEvent extends React.Component {
                 value={this.state.Item}
                 onChange={this.handleChange}
                 margin="normal"
+              /> */}
+
+              {/* <TextField
+                id="filled-multiline-static"
+                label="Event Name"
+                multiline
+                rows="6"
+                defaultValue=""
+                className={classes.textField}
+                margin="normal"
+                variant="filled"
+              /> */}
+
+              {/* <TextField
+                item xs = {6}
+                id="outlined-multiline-static"
+                label="Event Name"
+                multiline
+                rows="4"
+                defaultValue="Default Value"
+                className={classes.textField}
+                margin="normal"
+                variant="outlined"
+              /> */}
+
+              {/* <TextField
+                id="outlined-multiline-static"
+                label="Event Details"
+                multiline
+                rows="6"
+                defaultValue=""
+                className={classes.textField}
+                margin="normal"
+                variant="outlined"
+              /> */}
+
+              <TextField
+                id="outlined-name"
+                label="Name"
+                className={classes.textField}
+                // value={values.name}
+                // onChange={handleChange("name")}
+                margin="normal"
+                variant="outlined"
               />
+            </Grid>
 
-
-
+            <Grid field sm={12}>
+              <TextField
+                id="outlined-multiline-static"
+                label="Event Details"
+                multiline
+                rows="6"
+                defaultValue=""
+                className={classes.textField}
+                margin="normal"
+                variant="outlined"
+              />
             </Grid>
           </Grid>
         </Paper>
