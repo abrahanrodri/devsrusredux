6d5c6f5e5ce35f7672b353d917e85f948c4244f8
Sheriff Alhassan <dayo.hassan@gmail.com>
2019-09-06 17:47:09 -0400

    tutoring package.json work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const db = require('./models');
 
 app.use(bodyParser.urlencoded({ extended: true }));
